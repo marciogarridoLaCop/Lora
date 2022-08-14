c686aca0020ad0bdb33b3312add002cf413fae42
Marcio <marciogarrido@id.uff.br>
2022-08-13 22:19:50 -0300

    WIFI+GSM

---
+++ src/transmissao.h
@@ -0,0 +1,22 @@
+#include <WiFi.h>
+#include <api.h>
+#include <sensor.h>
+#include <gsm.h>
+
+
+void envio()
+{
+    inicializa_sensor();
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.println("Envio por Wifi");
+        wifi_http_post(coletadados("1"));
+       
+    }
+    else
+    {
+       Serial.println("Envio por GSM");
+       gsm_http_post(coletadados("1"));
+      
+    }
+}
